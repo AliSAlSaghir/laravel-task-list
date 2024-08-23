3a888ac1053184541b7de00c9ae5d9f60704e800
Ali AlSaghir <alisalsaghir@gmail.com>
2024-08-23 10:00:59 +0300

    add styling and complete the app

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,21 +3,21 @@
 @section('title', 'List of Tasks')
 
 @section('content')
-<div>
-  <a href="{{route('tasks.create')}}">Add Task</a>
-</div>
+    <nav class="mb-4">
+        <a href="{{ route('tasks.create') }}" class="link">Add Task</a>
+    </nav>
 
-@forelse($tasks as $task)
-<div>
-  <a href="{{ route('tasks.show', ['task' => $task->id])}}">{{ $task->title}}</a>
-</div>
-@empty
-<div>There are no tasks!</div>
-@endforelse
-@if ($tasks->count())
-<nav>
-  {{$tasks->links()}}
-</nav>
-@endif
+    @forelse($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}" @class(['line-through' => $task->completed])>{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+    @if ($tasks->count())
+        <nav class="mt-4">
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 
 @endsection
